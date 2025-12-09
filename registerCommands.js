1a4a08e7d362944896ff83b3f907d062fb5c00b3
yuuta5511 <yy274862@gmail.com>
2025-12-09 23:54:45 +0200

    Update registerCommands.js

--- registerCommands.js
+++ registerCommands.js
@@ -6,30 +6,48 @@ const commands = [
         .setDescription('Request a role for a project')
         .addStringOption(option =>
             option.setName('role')
-                .setDescription('The role type you need')
-                .setRequired(true)
-                .addChoices(
-                    { name: 'Editor (ED)', value: 'ED' },
-                    { name: 'Proofreader (PR)', value: 'PR' },
-                    { name: 'Translator KTL', value: 'KTL' },
-                    { name: 'Translator JTL', value: 'JTL' },
-                    { name: 'Translator CTL', value: 'CTL' },
-                ))
+            .setDescription('The role type you need')
+            .setRequired(true)
+            .addChoices(
+                { name: 'Editor (ED)', value: 'ED' },
+                { name: 'Proofreader (PR)', value: 'PR' },
+                { name: 'Translator KTL', value: 'KTL' },
+                { name: 'Translator JTL', value: 'JTL' },
+                { name: 'Translator CTL', value: 'CTL' },
+            ))
         .addRoleOption(option =>
             option.setName('for')
-                .setDescription('Select the project role')
-                .setRequired(true))
+            .setDescription('Select the project role')
+            .setRequired(true))
         .addIntegerOption(option =>
             option.setName('from')
-                .setDescription('Starting chapter number')
-                .setRequired(true)
-                .setMinValue(1))
+            .setDescription('Starting chapter number')
+            .setRequired(true)
+            .setMinValue(1))
         .addIntegerOption(option =>
             option.setName('number_of_chapters')
-                .setDescription('Number of chapters needed (optional)')
-                .setRequired(false)
-                .setMinValue(1))
-].map(command => command.toJSON());
+            .setDescription('Number of chapters needed (optional)')
+            .setRequired(false)
+            .setMinValue(1)),
+
+    new SlashCommandBuilder()
+        .setName('assign')
+        .setDescription('Assign a role and Drive access to a user')
+        .addUserOption(option =>
+            option.setName('user')
+            .setDescription('Select a user to assign')
+            .setRequired(true))
+        .addRoleOption(option =>
+            option.setName('to')
+            .setDescription('Select a role to assign (matching room name)')
+            .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('from')
+            .setDescription('Starting chapter number')
+            .setRequired(true)
+            .setMinValue(1))
+].map(cmd => cmd.toJSON());
+
 
 const rest = new REST().setToken(process.env.BOT_TOKEN);
 
